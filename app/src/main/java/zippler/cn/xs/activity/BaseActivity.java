d84ba3d4ad0f09c693ea0da42990f5995fcc649e
csuZipple <csu_zipple@qq.com>
2018-05-04 09:53:43 +0800

    add splash screen

---
+++ app/src/main/java/zippler/cn/xs/activity/BaseActivity.java
@@ -0,0 +1,45 @@
+package zippler.cn.xs.activity;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import zippler.cn.xs.util.ActivityCollection;
+import zippler.cn.xs.util.StateBarUtil;
+
+public class BaseActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollection.addActivity(this);
+        StateBarUtil.translucentStatusBar(this,true);//convert the status bar to transparency
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.hide();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (ActivityCollection.removeActivity(this)){
+            Log.d("info",this.getClass().getSimpleName()+"destroyed...");
+        }
+    }
+
+    /**
+     * 发布toast
+     * @param msg 信息
+     */
+    protected void toast(String msg){
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        //在此添加控件点击事件监听事件
+    }
+}
---
+++ app/src/main/java/zippler/cn/xs/activity/SplashActivity.java
@@ -0,0 +1,14 @@
+package zippler.cn.xs.activity;
+
+import android.os.Bundle;
+
+import zippler.cn.xs.R;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
